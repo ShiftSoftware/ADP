69eab148f8ec88a611437ac2d6ed6c9ef486fcda
Aza Asim <aza@shift.software>
2025-03-06 23:08:54 +0300

    Add More Detailed Properties to WarrantyClaimModel

---
+++ ADP.Models/Models/Enums/WarrantyManufacturerClaimStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ShiftSoftware.ADP.Models.Enums;
+
+public enum WarrantyManufacturerClaimStatus
+{
+    [Description("N/A")]
+    NA = 0,
+
+    [Description("Exported")]
+    Exported = 1,
+
+    [Description("Paid")]
+    Paid = 3,
+
+    [Description("Rejected")]
+    Rejected = 4,
+
+    [Description("On Hold")]
+    OnHold = 5
+}
