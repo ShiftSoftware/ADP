0e4e96c743784aeb8ef7918047598002dc3003c9
Aza Asim <aza@shift.software>
2025-04-30 18:44:55 +0300

    Single Feature for testing all 'Authorized & Unauthorized' Scenarios

---
+++ Lookup.Services.Features/Authorized.feature
@@ -0,0 +1,42 @@
+Feature: Authorized VehicleS
+Authorized & Unauthorized Cars
+	A car is considered "Authorized" or "Official" if it is imported by the official distributor for a specific geographic region.
+	In contrast, cars not imported by the official distributor are considered "Unauthorized", "Unofficial", or "Grey Imports".
+
+Background:
+	Given a dealer with the following vehicles as initial stock:
+		| VIN               |
+		| 1FMZU72E12UB00984 |
+		| 1FMCU0F73AKB12345 |
+
+	And the following vehicles in their dealer stock (coming from their DMS):
+		| VIN               |
+		| 1FDKF37GXVEB34368 |
+		| 1FTFW1EFXEKD12345 |
+
+	And the following vehicles in official SSC Vehciles (Provided by the vehicle manufacturer):
+		| VIN               |
+		| 1G1ZC5E17BF283048 |
+  
+Scenario: Authorized From Initial Stock
+	When Checking "1FMZU72E12UB00984"
+	Then The Vehicle is considered Authroized
+
+Scenario: Authorized From Dealer Stock (Dealer DMS)
+	When Checking "1FDKF37GXVEB34368"
+	Then The Vehicle is considered Authroized
+
+Scenario: Authorized From SSC
+	When Checking "1G1ZC5E17BF283048"
+	Then The Vehicle is considered Authroized
+
+
+Scenario: Unauthorized
+	When Checking "WMWZB3C55BWM46667"
+	Then The Vehicle is considered Unauthroized
+	When Checking "1FMYU60EXYUA30399"
+	Then The Vehicle is considered Unauthroized
+	When Checking "5GAKRBKD5EJ376173"
+	Then The Vehicle is considered Unauthroized
+	When Checking "3VWCD21Y33M352232"
+	Then The Vehicle is considered Unauthroized
---
+++ Lookup.Services.Features/AuthorizedStepDefinitions.cs
@@ -0,0 +1,97 @@
+using Reqnroll;
+using ShiftSoftware.ADP.Lookup.Services.Aggregate;
+using ShiftSoftware.ADP.Lookup.Services.Services;
+using ShiftSoftware.ADP.Models.Vehicle;
+using System.Text.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lookup.Services.Features;
+
+[Binding]
+public class AuthorizedStepDefinitions
+{
+    private string _vin = string.Empty;
+    private readonly CompanyDataAggregateCosmosModel _companyDataAggregate;
+    
+    private readonly ScenarioContext _scenarioContext;
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public AuthorizedStepDefinitions(ScenarioContext scenarioContext, ITestOutputHelper testOutputHelper)
+    {
+        _scenarioContext = scenarioContext;
+        _testOutputHelper = testOutputHelper;
+        this._companyDataAggregate = new();
+    }
+
+
+    [Given("a dealer with the following vehicles as initial stock:")]
+    public void GivenADealerWithTheFollowingVehiclesInInitialStock(DataTable dataTable)
+    {
+        this._testOutputHelper.WriteLine("ROw Count is: " + dataTable.Rows.Count);
+
+        var vins = dataTable.Rows.Select(x => x.Values.ElementAtOrDefault(0));
+
+        _companyDataAggregate.InitialOfficialVINs.AddRange(vins.Select(x => new InitialOfficialVINModel { VIN = x }));
+    }
+
+
+    [Given("the following vehicles in their dealer stock \\(coming from their DMS):")]
+    public void GivenTheFollowingVehiclesInDealerStock(DataTable dataTable)
+    {
+        var vins = dataTable.Rows.Select(x => x.Values.ElementAtOrDefault(0));
+
+        _companyDataAggregate.VehicleEntries.AddRange(vins.Select(x => new VehicleEntryModel { VIN = x }));
+    }
+
+
+    [Given("the following vehicles in official SSC Vehciles \\(Provided by the vehicle manufacturer):")]
+    public void GivenTheFollowingVehiclesInSsc(DataTable dataTable)
+    {
+        var vins = dataTable.Rows.Select(x => x.Values.ElementAtOrDefault(0));
+
+        _companyDataAggregate.SSCAffectedVINs.AddRange(vins.Select(x => new SSCAffectedVINModel { VIN = x }));
+    }
+
+
+    [When("Checking {string}")]
+    public void WhenCheckingWhichIsInInitialStock(string vin)
+    {
+        this._vin = vin;
+    }
+
+    [Then("The Vehicle is considered Authroized")]
+    public void ThenTheVehicleIsConsideredAuthroized()
+    {
+        var checker = new VehicleAuthorizationEvaluator();
+
+        var authorizedResult = checker.Evaluate(
+            _vin,
+            _companyDataAggregate
+        );
+
+        this._testOutputHelper.WriteLine("");
+        this._testOutputHelper.WriteLine("");
+        this._testOutputHelper.WriteLine("");
+
+        this._testOutputHelper.WriteLine("VIN is: " + _vin);
+        this._testOutputHelper.WriteLine("Data is: " + JsonSerializer.Serialize(_companyDataAggregate, new JsonSerializerOptions { WriteIndented = true }));
+
+        Assert.True(authorizedResult);
+    }
+
+
+    [Then("The Vehicle is considered Unauthroized")]
+    public void ThenTheVehicleIsConsideredUnauthroized()
+    {
+        var checker = new VehicleAuthorizationEvaluator();
+
+        var authorizedResult = checker.Evaluate(
+            _vin,
+            _companyDataAggregate
+        );
+
+        Assert.False(authorizedResult);
+    }
+
+}
