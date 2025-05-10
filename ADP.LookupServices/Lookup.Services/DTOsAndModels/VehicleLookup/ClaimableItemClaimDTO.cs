eb1b4fa2eb44cae87e5d4d13b22f3cc970edf7fa
AzaAsim <aza@shift.software>
2025-05-10 21:16:42 +0300

    Add ClaimableItemClaimDTO

---
+++ ADP.LookupServices/Lookup.Services/DTOsAndModels/VehicleLookup/ClaimableItemClaimDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftSoftware.ADP.Lookup.Services.DTOsAndModels.VehicleLookup;
+
+public class ClaimableItemClaimDTO
+{
+    public string Vin { get; set; }
+    public string Invoice { get; set; }
+    public string JobNumber { get; set; }
+    public VehicleSaleInformation SaleInformation { get; set; }
+    public VehicleServiceItemDTO ServiceItem { get; set; }
+    public VehicleServiceItemDTO[] CancelledServiceItems { get; set; } = [];
+}
