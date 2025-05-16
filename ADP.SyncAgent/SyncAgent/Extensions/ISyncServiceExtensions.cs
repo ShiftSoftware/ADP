315a5426d2a2a44d31345b320c010d7f1496978b
Nahro Fuad <nahropro@gmail.com>
2025-05-17 01:45:39 +0300

    Add logging capabilities to ISyncServiceExtensions

--- ADP.SyncAgent/SyncAgent/Extensions/ISyncServiceExtensions.cs
+++ ADP.SyncAgent/SyncAgent/Extensions/ISyncServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
 using ShiftSoftware.ADP.SyncAgent.Services.Interfaces;
 
 namespace ShiftSoftware.ADP.SyncAgent.Extensions;
@@ -12,6 +13,187 @@ public static class ISyncServiceExtensions
         where TDestination : class, new()
     {
         syncService.SetupMapping(x =>new ValueTask<IEnumerable<TDestination?>?>(mapper.Map<IEnumerable<TDestination>>(x)));
+
+        return syncService;
+    }
+
+
+    /// <summary>
+    /// Please add this after all other configurations, to avoid unexpected behaviors.
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDestination"></typeparam>
+    /// <param name="syncService"></param>
+    /// <param name="logger"></param>
+    /// <returns></returns>
+    public static ISyncService<TSource, TDestination> AddLogger<TSource, TDestination>(
+        this ISyncService<TSource, TDestination> syncService,
+        ILogger logger)
+        where TSource : class, new()
+        where TDestination : class, new()
+    {
+
+        if(syncService.Preparing is not null)
+        {
+            var previousPreparing = syncService.Preparing;
+            syncService.SetupPreparing ( async (x) =>
+            {
+                logger.LogInformation("Start preparing.");
+
+                var result = await previousPreparing(x);
+
+                logger.LogInformation("Preparing finished.");
+
+                return result;
+            });
+        }
+
+        if(syncService.SourceTotalItemCount is not null)
+        {
+            var previousSourceTotalItemCount = syncService.SourceTotalItemCount;
+            syncService.SetupSourceTotalItemCount ( async (x) =>
+            {
+                logger.LogInformation($"Start getting source total item count for {x.Input}");
+
+                var result = await previousSourceTotalItemCount(x);
+
+                logger.LogInformation($"Getting source total item count finished for {x.Input}");
+
+                return result;
+            });
+        }
+
+        if(syncService.GetSourceBatchItems is not null)
+        {
+            var previousGetSourceBatchItems = syncService.GetSourceBatchItems;
+            syncService.SetupGetSourceBatchItems ( async (x) =>
+            {
+                logger.LogInformation($"Start getting source items for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                var result = await previousGetSourceBatchItems(x);
+
+                logger.LogInformation($"Getting source batch items finished for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                return result;
+            });
+        }
+
+        if(syncService.Mapping is not null)
+        {
+            var previousMapping = syncService.Mapping;
+            syncService.SetupAdvancedMapping ( async (x) =>
+            {
+                logger.LogInformation($"Start mapping for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                var result = await previousMapping(x);
+
+                logger.LogInformation($"Mapping finished for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                return result;
+            });
+        }
+
+        if(syncService.StoreBatchData is not null)
+        {
+            var previousStoreBatchData = syncService.StoreBatchData;
+            syncService.SetupStoreBatchData ( async (x) =>
+            {
+                logger.LogInformation($"Start storing batch data for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                var result = await previousStoreBatchData(x);
+
+                logger.LogInformation($"Storing batch data finished for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+                return result;
+            });
+        }
+
+        var previousBatchRetry = syncService.BatchRetry;
+        syncService.SetupBatchRetry ( async (x) =>
+        {
+            RetryAction result = RetryAction.RetryAndStopAfterLastRetry;
+
+            if (previousBatchRetry is not null)
+                result = await previousBatchRetry(x);
+
+            if(result == RetryAction.RetryAndStopAfterLastRetry || result == RetryAction.RetryAndContinueAfterLastRetry)
+                logger.LogWarning($"Batch failed we do retry for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+            else if(result == RetryAction.Skip)
+                logger.LogWarning($"Batch failed we do skip for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+            else if(result == RetryAction.Stop)
+                logger.LogError($"Batch failed we do stop for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+            return result;
+        });
+
+        var previousBatchCompleted = syncService.BatchCompleted;
+        syncService.SetupBatchCompleted ( async (x) =>
+        {
+            bool result = true;
+
+            if(previousBatchCompleted is not null)
+                result = await previousBatchCompleted(x);
+
+            if(result)
+                logger.LogInformation($"Batch completed succesfully for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+            else
+                logger.LogError($"Batch failed for {x.Input.Status.ActionType}, step {x.Input.Status.CurrentStep + 1}" +
+                    (x.Input.Status.TotalSteps.HasValue ? $" of {x.Input.Status.TotalSteps}" : ""));
+
+            return result;
+        });
+
+        var previousActionCompleted = syncService.ActionCompleted;
+        syncService.SetupActionCompleted ( async (x) =>
+        {
+            bool result = true;
+            if(previousActionCompleted is not null)
+                result = await previousActionCompleted(x);
+
+            if(result)
+                logger.LogInformation($"Action {x.Input.ActionType} is completed successfully");
+            else
+                logger.LogError($"Action {x.Input.ActionType} is failed");
+
+            return result;
+        });
+
+        var previousFailed = syncService.Failed;
+        syncService.SetupFailed ( async () =>
+        {
+            if(previousFailed is not null)
+                await previousFailed();
+
+            logger.LogError("The sync operation is failed.");
+        });
+
+        var previousSucceeded = syncService.Succeeded;
+        syncService.SetupSucceeded ( async () =>
+        {
+            if(previousSucceeded is not null)
+                await previousSucceeded();
+
+            logger.LogInformation("The sync operation is succeeded.");
+        });
+
+        var previousFinished = syncService.Finished;
+        syncService.SetupFinished ( async () =>
+        {
+            if(previousFinished is not null)
+                await previousFinished();
+
+            logger.LogInformation("The sync operation is finished.");
+        });
 
         return syncService;
     }
