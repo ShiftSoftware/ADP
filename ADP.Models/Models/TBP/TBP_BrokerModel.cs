e4b649989342480cd58cb303d27080f0592a9814
Nahro Fuad <nahropro@gmail.com>
2025-05-19 18:27:24 +0300

    Enhance TBP_BrokerModel and related classes
    Updated TBP_BrokerModel to implement ICompanyProps and IRegionProps, adding new properties for Company and Region IDs. Refactored TBP_BrokerInvoice and TBP_BrokerVehicleTransfer to create and assign broker objects. Modified TBP_VSData to incorporate new properties and streamline broker stock handling. Added necessary using directives and improved null checks for better code clarity and functionality.

--- ADP.Models/Models/TBP/TBP_BrokerModel.cs
+++ ADP.Models/Models/TBP/TBP_BrokerModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ShiftSoftware.ADP.Models.TBP;
 
-public class TBP_BrokerModel
+public class TBP_BrokerModel : ICompanyProps, IRegionProps
 {
     public long ID { get; set; }
     public string Name { get; set; }
@@ -12,4 +12,8 @@ public class TBP_BrokerModel
     public string Location { get; set; }
     public bool IsDeleted { get; set; }
     public IEnumerable<string> AccountNumbers { get; set; }
+    public string CompanyID { get; set; }
+    public string CompanyHashID { get; set; }
+    public string RegionID { get; set; }
+    public string RegionHashID { get; set; }
 }
