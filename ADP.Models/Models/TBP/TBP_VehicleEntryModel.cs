7411c750e77cad766f0c73c65ec10aae407bbeea
Nahro Fuad <nahropro@gmail.com>
2025-05-25 15:45:37 +0300

    Refactor TBP_VehicleEntryModel properties
    Changed InvoiceDate type from DateTimeOffset? to DateTime?
    and added a new nullable Region property for better data handling.

--- ADP.Models/Models/TBP/TBP_VehicleEntryModel.cs
+++ ADP.Models/Models/TBP/TBP_VehicleEntryModel.cs
@@ -4,10 +4,11 @@ namespace ShiftSoftware.ADP.Models.TBP;
 
 public class TBP_VehicleEntryModel
 {
-    public DateTimeOffset? InvoiceDate { get; set; }
+    public DateTime? InvoiceDate { get; set; }
     public string CustomerAccountNumber { get; set; }
     public string Status { get; set; }
     public string LineStatus { get; set; }
     public string Location { get; set; }
     public long? CompanyID { get; set; }
+    public string? Region { get; set; }
 }
