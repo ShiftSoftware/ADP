1b98b38748007c833a5b15ae1bf1c2bf8424f97d
Kodo Yousif <kodo.yousif@gmail.com>
2025-06-22 09:24:47 +0300

    vehicle paint thickness migrated

--- ADP.WebComponents/adp-web-components/src/components/vehicle-lookup/vehicle-accessories.tsx
+++ ADP.WebComponents/adp-web-components/src/components/vehicle-lookup/vehicle-accessories.tsx
@@ -124,19 +124,19 @@ export class VehicleAccessories implements MultiLingual, VehicleInfoLayoutInterf
     ];
 
     const rows = accessories?.map(accessory => ({
-      partNumber: accessory.partNumber,
-      description: accessory.description,
+      partNumber: accessory?.partNumber,
+      description: accessory?.description,
       image: () => (
         <div class="size-[100px] flex mx-auto items-center justify-center">
           <button
-            onClick={({ target }) => openImageViewer.bind(this)(target as HTMLImageElement, accessory.image)}
+            onClick={({ target }) => openImageViewer.bind(this)(target as HTMLImageElement, accessory?.image)}
             class="shrink-0 relative ring-0 outline-none w-fit mx-auto [&_img]:hover:shadow-lg [&_div]:hover:!opacity-100 cursor-pointer"
           >
             <div class="absolute flex-col justify-center gap-[4px] size-full flex items-center pointer-events-none hover:opacity-100 rounded-lg opacity-0 bg-black/40 transition-all duration-300">
               <img src={Eye} />
               <span class="text-white">{texts.expand}</span>
             </div>
-            <img class="w-auto h-auto max-w-[100px] max-h-[100px] cursor-pointer shadow-sm rounded-lg transition-all duration-300" src={accessory.image} />
+            <img class="w-auto h-auto max-w-[100px] max-h-[100px] cursor-pointer shadow-sm rounded-lg transition-all duration-300" src={accessory?.image} />
           </button>
         </div>
       ),
