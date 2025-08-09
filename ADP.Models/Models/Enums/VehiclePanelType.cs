185c9de0a5f712a7fab0dd35c9967cc14cdb0814
Nahro Fuad <nahropro@gmail.com>
2025-08-10 00:56:32 +0300

    Add VehiclePanelType enum and update PaintThickness model

---
+++ ADP.Models/Models/Enums/VehiclePanelType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ShiftSoftware.ADP.Models.Enums;
+
+public enum VehiclePanelType
+{
+    [Description("Door")]
+    Door = 1,
+
+    [Description("Fender")]
+    Fender = 2,
+
+    [Description("Roof")]
+    Roof = 3,
+
+    [Description("Hood")]
+    Hood = 4,
+
+    [Description("Tail Gate")]
+    TailGate = 5,
+}
--- ADP.Models/Models/Vehicle/PaintThicknessInspectionPanelModel.cs
+++ ADP.Models/Models/Vehicle/PaintThicknessInspectionPanelModel.cs
@@ -5,10 +5,10 @@ namespace ShiftSoftware.ADP.Models.Vehicle;
 
 public class PaintThicknessInspectionPanelModel
 {
-    public string Name { get; set; }
+    public VehiclePanelType PanelType { get; set; }
     public decimal MeasuredThickness { get; set; }
     public VehiclePanelSide? PanelSide { get; set; }
     public VehiclePanelPosition? PanelPosition { get; set; }
-    public IEnumerable<string> Images { get; set; }
+    public IEnumerable<string> Images { get; set; } = [];
 }
 
