edcc4991b67c94d571411a31b96fb34332e3bdd8
AzaAsim <aza@shift.software>
2025-10-11 08:13:35 +0300

    Vehicle Lookup Services: Extract Sale Information to a dedicated Evaluator

--- ADP.LookupServices/Lookup.Services/Services/VehicleLookupService.cs
+++ ADP.LookupServices/Lookup.Services/Services/VehicleLookupService.cs
@@ -68,26 +68,21 @@ public class VehicleLookupService
             SSC = new VehicleSSCEvaluator(companyDataAggregate).Evaluate(),
             NextServiceDate = companyDataAggregate.Invoices?.OrderByDescending(x => x.InvoiceDate).FirstOrDefault()?.NextServiceDate,
             Accessories = await GetAccessories(),
-
+            SaleInformation = await new VehicleSaleInformationEvaluator(companyDataAggregate, options, services, lookupCosmosService).Evaluate(languageCode),
         };
 
-        DateTime? warrantyStartDate = null;
         DateTime? freeServiceStartDate = null;
-
-        var serviceActivation = companyDataAggregate.VehicleServiceActivations.FirstOrDefault();
 
         if (vehicle is not null)
         {
-            // Set SaleInformation
-            data.SaleInformation = await GetSaleInformationAsync(companyDataAggregate.VehicleEntries);
+            DateTime? warrantyStartDate = null;
 
             //Normal company Sale
             if (data.SaleInformation?.Broker is null)
             {
                 warrantyStartDate = data.SaleInformation?.WarrantyActivationDate;
 
-                if (serviceActivation is not null)
-                    warrantyStartDate = serviceActivation.WarrantyActivationDate;
+                warrantyStartDate = companyDataAggregate.VehicleServiceActivations.FirstOrDefault()?.WarrantyActivationDate;
 
                 if (warrantyStartDate is null && options.WarrantyStartDateDefaultsToInvoiceDate)
                     warrantyStartDate = data.SaleInformation?.InvoiceDate;
@@ -125,7 +120,7 @@ public class VehicleLookupService
             companyDataAggregate.PaidServiceInvoices,
             companyDataAggregate.ItemClaims,
             companyDataAggregate.VehicleInspections,
-            serviceActivation
+            companyDataAggregate.VehicleServiceActivations.FirstOrDefault()
         );
 
         if (insertSSCLog)
@@ -227,97 +222,6 @@ public class VehicleLookupService
             return await options.PaintThickneesImageUrlResolver(new (image,languageCode,services));
 
         return image;
-    }
-
-    private async Task<VehicleSaleInformation> GetSaleInformationAsync(List<VehicleEntryModel> vehicles)
-    {
-        VehicleSaleInformation result = new();
-
-        if (!(vehicles?.Any() ?? false))
-            return null;
-
-        var vsData = vehicles
-            .OrderByDescending(x => x.InvoiceDate == null)
-            .ThenByDescending(x => x.InvoiceDate)
-            .FirstOrDefault();
-
-        result.InvoiceDate = vsData?.InvoiceDate;
-        result.WarrantyActivationDate = vsData?.WarrantyActivationDate;
-        result.Status = vsData.OrderStatus;
-        result.Location = vsData.Location;
-        result.SaleType = vsData.SaleType;
-        result.AccountNumber = vsData.AccountNumber;
-        //result.RegionID = vsData.RegionID;
-
-        result.InvoiceNumber = vsData?.InvoiceNumber;
-        result.InvoiceTotal = vsData?.InvoiceTotal ?? 0;
-        //result.CompanyID = vsData?.CompanyID;
-        //result.BranchID = vsData?.BranchID;
-
-        result.CustomerID = vsData?.CustomerID;
-        result.CustomerAccountNumber = vsData?.CustomerAccountNumber;
-
-        if (options.CountryFromBranchIDResolver is not null)
-        {
-            var countryResult = await options.CountryFromBranchIDResolver(new(vsData.BranchID, languageCode, services));
-
-            if (countryResult is not null)
-            {
-                result.CountryID = countryResult.Value.countryID?.ToString();
-                result.CountryName = countryResult.Value.countryName;
-            }
-        }
-
-        if (options.CompanyNameResolver is not null)
-            result.CompanyName = await options.CompanyNameResolver(new(vsData.CompanyID, languageCode, services));
-
-        if (options.CompanyBranchNameResolver is not null)
-            result.BranchName = await options.CompanyBranchNameResolver(
-                new(vsData.BranchID, languageCode, services));
-
-        string? companyLogo = null;
-
-        if(options.CompanyLogoResolver is not null)
-            companyLogo = await options.CompanyLogoResolver(new(vsData.CompanyID, languageCode, services));   
-
-        //if(!string.IsNullOrWhiteSpace(companyLogo))
-        //    try
-        //    {
-        //        result.CompanyLogo = await GetCompanyLogo(JsonSerializer.Deserialize<List<ShiftFileDTO>>(companyLogo));
-        //    }
-        //    catch (Exception){}
-
-        if (companyDataAggregate.BrokerInvoices?.Any() ?? false)
-        {
-            var brokerInvoice = companyDataAggregate.BrokerInvoices.FirstOrDefault();
-            var broker = await lookupCosmosService.GetBrokerAsync(brokerInvoice.ID);
-
-            result.Broker = new VehicleBrokerSaleInformation
-            {
-                BrokerID = brokerInvoice.ID,
-                BrokerName = broker?.Name,
-                CustomerID = (brokerInvoice.BrokerCustomerID ?? brokerInvoice.NonOfficialBrokerCustomerID) ?? 0,
-                InvoiceDate = brokerInvoice.InvoiceDate,
-                InvoiceNumber = brokerInvoice.InvoiceNumber,
-            };
-        }
-        else
-        {
-            var broker = await lookupCosmosService.GetBrokerAsync(vsData?.CustomerAccountNumber, vsData?.CompanyID);
-
-            // If vehicle sold to broker and the broker is terminated, then make vsdata as start date.
-            // If vehicle sold to broker before start date and it is not exists in broker intial vehicles,
-            // then make vsdata as start date.
-            if (broker is not null)
-                if (!broker.TerminationDate.HasValue && (broker.AccountStartDate <= vsData?.InvoiceDate || companyDataAggregate.BrokerInitialVehicles?.Count(x => x?.BrokerID == broker.ID) > 0))
-                    result.Broker = new VehicleBrokerSaleInformation
-                    {
-                        BrokerID = broker.ID,
-                        BrokerName = broker.Name
-                    };
-        }
-
-        return result;
     }
 
     private async Task<List<ShiftFileDTO>> GetCompanyLogo(List<ShiftFileDTO> companyLogo)
