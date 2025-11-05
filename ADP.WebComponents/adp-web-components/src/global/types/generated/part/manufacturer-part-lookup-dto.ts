d376a5949c3750b52bfc51367929df4e4eb8c0ed
Nahro Fuad <nahropro@gmail.com>
2025-11-05 14:59:00 +0300

    Refactor Manufacturer Lookup Models and DTOs

--- ADP.WebComponents/adp-web-components/src/global/types/generated/part/manufacturer-part-lookup-dto.ts
+++ ADP.WebComponents/adp-web-components/src/global/types/generated/part/manufacturer-part-lookup-dto.ts
@@ -1,6 +1,5 @@
-import type { ManufacturerResultDTO } from './manufacturer-result-dto';
 export type ManufacturerPartLookupDTO = {
     isSuccess: boolean;
     message?: string;
-    manufacturerResult?: ManufacturerResultDTO[];
+    manufacturerResult?: string[];
 };
