c6bb88718cc289d5ddb12117147e9f10ac0016fb
Nahro Fuad <nahropro@gmail.com>
2025-11-06 07:39:02 +0300

    Make StartDate and EndDate nullable in ExtendedWarrantyModel

--- ADP.Models/Models/Vehicle/ExtendedWarrantyModel.cs
+++ ADP.Models/Models/Vehicle/ExtendedWarrantyModel.cs
@@ -6,8 +6,8 @@ public class ExtendedWarrantyModel : IPartitionedItem, ICompanyProps
 {
     public string id { get; set; }
     public string VIN { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
 
     /// <summary>
     /// Extended Warranty Entries may go through a long approval process until considered active. Or they may be revoked after issuing.
