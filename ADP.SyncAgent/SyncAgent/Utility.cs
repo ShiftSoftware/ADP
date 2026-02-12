a36db5d549820dd03217e296f32aa08b2e959427
Nahro Fuad <nahropro@gmail.com>
2026-02-12 17:21:14 +0300

    Increase Cosmos DB throughput to 25,000 RU/s on creation

--- ADP.SyncAgent/SyncAgent/Utility.cs
+++ ADP.SyncAgent/SyncAgent/Utility.cs
@@ -128,17 +128,17 @@ public class Utility
     {
         var companyDatabaseResponse = await client.CreateDatabaseIfNotExistsAsync(
         Models.Constants.NoSQLConstants.Databases.CompanyData,
-            ThroughputProperties.CreateManualThroughput(1000)
+            ThroughputProperties.CreateManualThroughput(25000)
         );
 
         var logsDatabaseResponse = await client.CreateDatabaseIfNotExistsAsync(
             Models.Constants.NoSQLConstants.Databases.Logs,
-            ThroughputProperties.CreateManualThroughput(1000)
+            ThroughputProperties.CreateManualThroughput(25000)
         );
 
         var serviceDatabaseResponse = await client.CreateDatabaseIfNotExistsAsync(
             Models.Constants.NoSQLConstants.Databases.Services,
-            ThroughputProperties.CreateManualThroughput(1000)
+            ThroughputProperties.CreateManualThroughput(25000)
         );
 
         var companyDatabase = companyDatabaseResponse.Database;
