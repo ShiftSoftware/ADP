9e5f899056778410dba062a6f8dad126338c834f
Nahro Fuad <nahropro@gmail.com>
2026-02-14 15:09:56 +0300

    Rename ModificationDate to LastSaveDate in DuckDB model

--- ADP.Models/Models/Vehicle/DuckDBModels/WarrantyClaimDuckDBModel.cs
+++ ADP.Models/Models/Vehicle/DuckDBModels/WarrantyClaimDuckDBModel.cs
@@ -5,5 +5,5 @@ namespace ShiftSoftware.ADP.Models.Vehicle.DuckDBModels;
 public class WarrantyClaimDuckDBModel : WarrantyClaimModel
 {
     public new Guid id { get; set; }
-    public DateTime? ModificationDate { get; set; }
+    public DateTime? LastSaveDate { get; set; }
 }
