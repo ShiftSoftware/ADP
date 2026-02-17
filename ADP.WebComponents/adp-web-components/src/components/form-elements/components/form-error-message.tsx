df0a7c9d165e7dc74da3f4117e1279a9a3e67d0a
Kodo Yousif <kodo.yousif@gmail.com>
2026-02-17 20:33:26 +0300

    update service booking paths

--- ADP.WebComponents/adp-web-components/src/components/form-elements/components/form-error-message.tsx
+++ ADP.WebComponents/adp-web-components/src/components/form-elements/components/form-error-message.tsx
@@ -11,7 +11,7 @@ interface FormErrorMessageProps {
 export const FormErrorMessage: FunctionalComponent<FormErrorMessageProps> = ({ errorMessage, isError, name }) => (
   <div part={cn('form-error-message-container', { [`${name}-error-message-container`]: !!name })} class="error-message-container">
     <div
-      part={cn('form-error-message', { [`${name}-error-message`]: !!name })}
+      part={cn('form-error-message', { [`${name}-error-message`]: !!name, 'display-error-message': isError })}
       class={cn('error-message', {
         'display-error-message': isError,
       })}
