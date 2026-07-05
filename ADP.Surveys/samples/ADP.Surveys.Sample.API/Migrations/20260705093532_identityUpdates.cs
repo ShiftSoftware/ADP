e9676ca9aa190cf505fdae82985ec0ffb45e953e
Aza Asim <azaasim@gmail.com>
2026-07-05 12:39:03 +0300

    ADP.Surveys: Add framework change migrations

---
+++ ADP.Surveys/samples/ADP.Surveys.Sample.API/Migrations/20260705093532_identityUpdates.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShiftSoftware.ADP.Surveys.Sample.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class identityUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSuperAdmin",
+                schema: "ShiftIdentity",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuperAdmin",
+                schema: "ShiftIdentity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
